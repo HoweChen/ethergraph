e432f058f66c31a3ee99f2170c8e8b1112458c3d
SkyFather <cyh278889789@gmail.com>
2018-05-23 00:24:46 +0800

    update message contract and some build contracts

--- contracts/Message.sol
+++ contracts/Message.sol
@@ -1,39 +1,36 @@
 pragma solidity ^0.4.0;
+//pragma experimental ABIEncoderV2;
 
 contract Message {
 
-    string public nick_name;
-    string private message;
     address owner;
 
-    modifier restricted() {
+    struct Article {
+        address authorAddress;
+        string authorNickName;
+        string title;
+        string body;
+    }
+
+    mapping(bytes32 => Article) ID2Article;
+
+    modifier isOwner(){
         if (msg.sender == owner) _;
     }
 
-    constructor() public{
+    constructor(){
         owner = msg.sender;
-
     }
 
-    function publish(string content) public restricted {
-        bytes memory temp = bytes(message);
-        if (temp.length == 0) {
-            message = content;
-        }
-        delete temp;
+    function publish(string authorNickName, string title, string body) public returns (bytes32){
+        Article memory temp = Article(msg.sender, authorNickName, title, body);
+        bytes32 memory tmpID = sha256(abi.encodePacked(msg.sender, authorNickName, title, body));
+        ID2Article[tmpID] = temp;
+        return (tmpID);
     }
 
-    function setNickName(string name) public restricted {
-        bytes memory temp = bytes(nick_name);
-        if (temp.length == 0) {
-            nick_name = name;
-        }
-        delete temp;
+    function getArticle(bytes32 articleID) public view returns (address, string, string, string){
+        Article memory tmp = ID2Article[articleID];
+        return (tmp.authorAddress, tmp.authorNickName, tmp.title, tmp.body);
     }
-
-    function getMessage() public view returns (string){
-        return message;
-    }
-
-
 }
