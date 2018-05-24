a0fe72ed3d4b9e7f40667257f82144a86112c3be
SkyFather <cyh278889789@gmail.com>
2018-05-24 15:31:04 +0800

    update contract

--- contracts/Message.sol
+++ contracts/Message.sol
@@ -24,13 +24,20 @@ contract Message {
 
     function publish(string authorNickName, string title, string body) public returns (bytes32){
         Article memory temp = Article(msg.sender, authorNickName, title, body);
-        bytes32 tmpID = sha256(abi.encodePacked(msg.sender, authorNickName, title, body));
+        //bytes32 tmpID = sha256(abi.encodePacked(msg.sender, authorNickName, title, body));
+        // only encode two argument, the nick name and the title for better usage of searching through the web url
+        bytes32 tmpID = sha256(abi.encodePacked(authorNickName, title));
         ID2Article[tmpID] = temp;
         return (tmpID);
     }
 
-    function getArticle(bytes32 articleID) public view returns (address, string, string, string){
+    function getArticleByID(bytes32 articleID) public view returns (address, string, string, string){
         Article memory tmp = ID2Article[articleID];
         return (tmp.authorAddress, tmp.authorNickName, tmp.title, tmp.body);
+    }
+
+    function getArticleByURL(string nickName, string title) public view returns (address, string, string, string){
+        bytes32 tmpID = sha256(abi.encodePacked(nickName, title));
+        return getArticleByID(tmpID);
     }
 }
